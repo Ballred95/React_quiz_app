8b649f3d503fba56039e104199c1ae089b2b1201
Ballred95 <blake.allred95@icloud.com>
2020-06-22 23:11:37 -0600

    created state and set state function

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import Qbank from './qbank'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      questionbank: []
+    }
+  }
   render() {
     return (
       <div className='app'>
